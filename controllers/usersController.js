e1a24f1269f694e8bf2cfb95e9256344da8c6cc9
Nur Kocar <64444237+nurkocar@users.noreply.github.com>
2020-12-06 22:19:39 +0000

    add new views controllers models

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,47 @@
-exports.get_users = function (req, res, next) {
-  res.render("users");
+const UserModel = require("../models/User");
+
+exports.get_users = async (req, res, next) => {
+  //get users from db
+  try {
+    const userList = await UserModel.findAll({});
+    res.render("users", { userList });
+  } catch (error) {
+    res.send("An error occured");
+  }
 };
+
+//on get request
+exports.show_add_user_form = (req, res) => {
+  res.render("addUser");
+};
+
+//on post request
+exports.add_user = async (req, res) => {
+  //add to db
+  try{
+    const newUser = await UserModel.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName
+    });
+    res.redirect('/users')
+  }catch(err){
+    //console.log('error', err)
+    res.send('An error occured.')
+  }
+};
+
+exports.delete_user = async (req, res) => {
+  try{
+    await UserModel.destroy({
+      where: {
+        id: req.params.id,
+      }
+    })
+    res.redirect('/users');
+  }catch (error) {
+    console.log('error', error);
+  }
+}
+
+// update({firstName: req.body.first_name}, {where: { id: req.params.id}
+
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ const usersController = require("../controllers/usersController");
 
 router.get("/", usersController.get_users);
 
+router.get("/add", usersController.show_add_user_form);
+router.post('/add', usersController.add_user)
+
+router.get('/:id/delete', usersController.delete_user);
+
 module.exports = router;
